b3b1431e662d9a3f9a1f5cee1a49f4aa7f77be65
Andrew Bjonnes <andrew@abjonnes.com>
2016-08-05 17:57:01 -0400

    expand documentation

--- bamtail.py
+++ bamtail.py
@@ -8,6 +8,11 @@ import sys
 
 
 def parse_arguments():
+    """Parse the command line arguments.
+
+    Returns:
+        Parsed arguments.
+    """
     parser = argparse.ArgumentParser(description='tail for BAMs')
     parser.add_argument(
         'filenames',
@@ -15,23 +20,38 @@ def parse_arguments():
         nargs='+',
         metavar='FILE'
     )
-
     return parser.parse_args()
 
 
 def gunzip_chunk(byte_stream):
-    """interpret a byte string as a gzipped chunk and return its
-    unzipped contents
+    """Interpret a byte string as a gzipped chunk and return its
+    unzipped contents.
+
+    Args:
+        byte_stream (bytes): Complete chunk of gzipped data to be ungzipped.
+
+    Returns:
+        Bytes representing the ungzipped chunk.
     """
     byte_stream = StringIO.StringIO(byte_stream)
     return gzip.GzipFile(fileobj=byte_stream).read()
 
 
 def get_bam_chunk(filename, size, tailsize=100000):
-    """find the final (or, in rare cases, penultimate) gzipped chunk in the BAM
-    file and return its unzipped contents
-    """
+    """Find the final (or, in rare cases, penultimate) gzipped chunk in the BAM
+    file and return its unzipped contents.
 
+    Args:
+        filename: Path of BAM.
+        size (int): Total filesize of BAM.
+        tailsize (int, optional): Number of bytes to look at from the end of
+            the BAM file, defaulting to 100000. This should be large enough
+            to read an entire unbroken gzipped chunk (64k?).
+
+    Returns:
+        Bytes representing the ungzipped final complete chunk of data in the
+            BAM file.
+    """
     with open(filename, 'rb') as bamfile:
         bamfile.seek(size-tailsize)
         tail = bamfile.read()
@@ -44,10 +64,17 @@ def get_bam_chunk(filename, size, tailsize=100000):
 
 
 def final_record_position(bam_data):
-    """find the reference index and position of the final record in the BAM
-    chunk
-    """
+    """Find the reference index and position of the final record in the BAM
+    chunk.
 
+    Args:
+        bam_data (bytes): Ungzipped contents of a chunk of BAM data.
+
+    Returns:
+        A tuple of ints, where the first int is the index of the reference
+            sequence (from the header) or -1 for an unmapped record, and the
+            second int is the position.
+    """
     while True:
         block_size, ref_id, pos = struct.unpack_from('<3i', bam_data)
         bam_data = bam_data[block_size+4:]
@@ -58,10 +85,17 @@ def final_record_position(bam_data):
 
 
 def get_bam_ref_sequences(filename, headsize=100000):
-    """read the list of reference sequence names from the beginning of the BAM
-    file
-    """
+    """Read the list of reference sequence names from the beginning of the BAM
+    file.
 
+    Args:
+        filename: Path of BAM to get the reference sequences from.
+        headsize (int, optional): number of bytes to read so that the complete
+        reference sequence data is included. Defaults to 10000.
+
+    Returns:
+        List of reference sequence names.
+    """
     with open(filename, 'rb') as bamfile:
         head = bamfile.read(headsize)
     fields = struct.unpack_from('<4BI2BH2B2H', head)
@@ -83,10 +117,17 @@ def get_bam_ref_sequences(filename, headsize=100000):
 
 
 def process_file(filename):
-    """find the chromosome and position of the final record in the final
-    chunk of the BAM file
-    """
+    """Find the chromosome and position of the final record in the final
+    chunk of the BAM file.
 
+    Args:
+        filename: Path of BAM on which to perform tail.
+
+    Returns:
+        String of the position of the final record of the BAM, e.g. 'chr1:100',
+            or 'unmapped' if the last record is not mapped to a reference
+            sequence.
+    """
     ref_sequences = get_bam_ref_sequences(filename)
     size = os.path.getsize(filename)
     bam_data = get_bam_chunk(filename, size)
@@ -100,8 +141,12 @@ def process_file(filename):
 
 
 def main(filenames):
-    """perform the tail operation on all given filenames"""
+    """Perform the tail operation on all given BAMs. If more than one BAM is
+    given, prefix the output with the filename.
 
+    Args:
+        filenames: A sequence of paths to BAMs on which to perform tail.
+    """
     for filename in filenames:
         if len(filenames) > 1:
             print '{}: '.format(filename),
